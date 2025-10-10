24ebf95585a6608792dc5cd7329b7035d4b360f7
chounan_ma <johnny880319@gmail.com>
2025-10-10 14:12:07 +0800

    fix: Resolve logo text rendering errors on mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
           src="/images/logos/my_blog_logo.svg"
           alt="My Logo"
           width={500}
